8e020aa34d5ed5e2d2988fb3237c152646eae32b
crDahaka <bkristian7@gmail.com>
2018-07-15 13:16:06 +0300

    Project initialization

---
+++ src/LearningSystem.Data/LearningSystemDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace LearningSystem.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    using LearningSystem.Data.Models;
+
+    public class LearningSystemDbContext : IdentityDbContext<User>
+    {
+        public LearningSystemDbContext(DbContextOptions<LearningSystemDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
